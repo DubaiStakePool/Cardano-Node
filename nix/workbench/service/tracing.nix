95d8789af249c764197bfbb529e26c0c8b51b4f1
Yupanqui <jnf@arcor.de>
2023-05-19 14:42:17 +0200

    workbench: config changes

--- nix/workbench/service/tracing.nix
+++ nix/workbench/service/tracing.nix
@@ -13,32 +13,65 @@ let
   {
     UseTraceDispatcher   = true;
 
-    ## Please see the generated tracing configuration reference at:
-    ##
-    ## https://github.com/input-output-hk/cardano-node/blob/master/doc/new-tracing/tracers_doc_generated.md#trace-messages
-    ##
+  ## Please see the generated tracing configuration reference at:
+  ##
+  ## https://github.com/input-output-hk/cardano-node/blob/master/doc/new-tracing/tracers_doc_generated.md#trace-messages
+  ##
     TraceOptions  = {
       "" =
         { severity = "Notice";
           backends = [
             "Stdout MachineFormat"
             "EKGBackend"
-          ] ++ optional tracer
-            "Forwarder";
+            ] ++ optional tracer
+              "Forwarder";
         };
 
-      ## These are benchmarking-specific config deviations from the default.
+      ## These are comparision-specific config deviations from the default.
       ##
-      "BlockFetch".severity = "Debug";
-      "BlockFetch.Client.CompletedBlockFetch".maxFrequency = 0;
-      "ChainSync".severity = "Debug";
-      "ChainSync.Client.DownloadedHeader".maxFrequency = 0;
+      ## "Resources".backends = ["EKGBackend"];
+      "Net.Net.AcceptPolicy".severity = "Info";
+      "ChainDB".severity = "Info";
+      "Net.ConnectionManager.Local".severity = "Info";
+      "Net.ConnectionManager.Remote".severity = "Info";
+      "Net.DNSResolver".severity = "Info";
+      "Net.Subscription.DNS".severity = "Info";
+      "Net.Startup.DiffusionInit".severity = "Info";
+      "Net.ErrorPolicy.Remote".severity = "Info";
+      "Forge.Loop".severity = "Info";
+      "Net.Subscription.IP".severity = "Info";
+      "Net.InboundGovernor".severity = "Info";
+      "Net.ErrorPolicy.Local".severity = "Info";
+      "Net.Peers".severity = "Info";
+      "Mempool".severity = "Info";
+      "Net.PeerSelection".severity = "Info";
+      "Net.Peers.PublicRoot".severity = "Info";
+      "Net.Server".severity = "Info";
+
+      "BlockFetch.Client.CompletedBlockFetch".severity = "Debug";
+      "BlockFetch.Client.SendFetchRequest".severity = "Debug";
+      "BlockFetch.Server.SendBlock".severity = "Debug";
+      "ChainDB.AddBlockEvent.AddedToCurrentChain".severity = "Debug";
+      "ChainDB.LedgerEvent.TookSnapshot".severity = "Debug";
+      "ChainSync.Client.DownloadedHeader".severity = "Debug";
+      "ChainSync.ServerHeader.Update".severity = "Debug";
+      "Forge.Loop.AdoptedBlock".severity = "Debug";
       "Forge.Loop.BlockContext".severity = "Debug";
+      "Forge.Loop.ForgedBlock".severity = "Debug";
       "Forge.Loop.LedgerState".severity = "Debug";
       "Forge.Loop.LedgerView".severity = "Debug";
       "Forge.Loop.MempoolSnapshot".severity = "Debug";
+      "Forge.Loop.NodeIsLeader".severity = "Debug";
+      "Forge.Loop.NodeNotLeader".severity = "Debug";
+      "Forge.Loop.StartLeadershipCheck".severity = "Debug";
       "Forge.Loop.TickedLedgerState".severity = "Debug";
-      "Startup".severity = "Debug";
+      "Mempool.AddedTx".severity = "Debug";
+      "Mempool.RejectedTx".severity = "Debug";
+      "Mempool.RemoveTxs".severity = "Debug";
+      "TraceBenchTxSubServAck".severity = "Debug";
+      "TraceBenchTxSubSummary".severity = "Debug";
+      "TraceTxSubmissionCollected".severity = "Debug";
+      "TraceTxSubmissionProcessed".severity = "Debug";
     };
   };
 
